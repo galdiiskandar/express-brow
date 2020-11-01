0a558e69b70713a59b91b6e64529005f3fa59f1b
Putra Dinata <putradnata15@gmail.com>
2020-11-01 14:59:57 +0800

    new feature

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Session;
+use Illuminate\Support\Facades\Redirect;
 
 
 class LoginController extends Controller
@@ -48,27 +50,29 @@ class LoginController extends Controller
     public function login(Request $request)
 {
     $this->validate($request, [
-        'username' => 'required|string', //VALIDASI KOLOM USERNAME
-        //TAPI KOLOM INI BISA BERISI EMAIL ATAU USERNAME
+        //username validation
+        'username' => 'required|string',
         'password' => 'required|string',
     ]);
 
-    //LAKUKAN PENGECEKAN, JIKA INPUTAN DARI USERNAME FORMATNYA ADALAH EMAIL, MAKA KITA AKAN MELAKUKAN PROSES AUTHENTICATION MENGGUNAKAN EMAIL, SELAIN ITU, AKAN MENGGUNAKAN USERNAME
-    $loginType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+    //do validation, if input is username, it will use username, unless this will use email
+    $loginType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email_user' : 'username';
 
-    //TAMPUNG INFORMASI LOGINNYA, DIMANA KOLOM TYPE PERTAMA BERSIFAT DINAMIS BERDASARKAN VALUE DARI PENGECEKAN DIATAS
     $login = [
         $loginType => $request->username,
         'password' => $request->password
     ];
 
-    //LAKUKAN LOGIN
+    // login validate
     if (auth()->attempt($login)) {
-        //JIKA BERHASIL, MAKA REDIRECT KE HALAMAN HOME
-        return redirect()->route('home');
+        //if success
+        return redirect()->route('barang.index');
 
     }
-    //JIKA SALAH, MAKA KEMBALI KE LOGIN DAN TAMPILKAN NOTIFIKASI
-    return redirect()->route('login')->with(['error' => 'Email/Password salah!']);
+    //else, will show flash notification
+    // return redirect()->route('login')->with(['error' => 'Email/Password salah!']);
+    Session::flash('error', 'Invalid Username or Password !');
+
+    return Redirect::to('login');
 }
 }
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -69,128 +68,27 @@
         <a class="navbar-brand" href="#" data-jq-dropdown="#jq-dropdown-1">
             <img class="pr-3 float-left" src="{{ asset('assets/img/logo-icon.png')}}" srcset="assets/img/logo-icon@2x.png 2x"  alt=""/>
             <div class="float-left">
-                <div>DashLab <i class="fa fa-caret-down pl-2"></i></div>
+                <div>Tiara </div>
                 <span class="page-direction f12 weight300">
-                    <span>home</span>
+                    <?php
+                        $fullUrl = url()->current();
+
+                        $exploded = explode('/',$fullUrl);
+
+                    ?>
+
+                    <span>{{ $exploded['3'] }}</span>
                     <i class="fa fa-angle-right"></i>
-                    <span>ui element</span>
+                    <span>{{ $exploded['4'] }}</span>
+
+                    @if (count($exploded) > 5)
+                        <i class="fa fa-angle-right"></i>
+                        <span>{{ $exploded['5'] }}</span>
+                    @endif
                 </span>
             </div>
         </a>
         <!--/brand name-->
-
-        <!--brand mega menu-->
-        <div id="jq-dropdown-1" class="jq-dropdown">
-            <div class="b-mega-menu">
-                <div class="card card-shadow">
-                    <div class="card-header p-2 pl-3">
-                        <div class="navbar-brand mt-2">
-                            <img class="pr-3 float-left" src="{{ asset('assets/img/logo-icon.png')}}" srcset="assets/img/logo-icon@2x.png 2x"  alt=""/>
-                            <div class="float-left">
-                                <div>DashLab</div>
-                                <span class="page-direction f12 weight300">
-                                    <span>home</span>
-                                    <i class="fa fa-angle-right"></i>
-                                    <span>ui element</span>
-                                </span>
-                            </div>
-                        </div>
-
-                        <div class="widget-action-link">
-                            <ul class="nav nav-tabs wal-nav-tabs" id="myTab" role="tablist">
-                                <li class="nav-item">
-                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true"><i class="fa fa-home"></i></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" id="analytics-tab" data-toggle="tab" href="#analytics" role="tab" aria-controls="analytics" aria-selected="false"><i class="fa fa-desktop"></i></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" id="application-tab" data-toggle="tab" href="#application" role="tab" aria-controls="application" aria-selected="false"><i class="fa fa-magnet"></i></a>
-                                </li>
-                            </ul>
-
-                        </div>
-                    </div>
-                    <div class="card-body p-0 ">
-                        <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                <div class="row no-gutters">
-                                    <div class="col-4 border-right">
-                                        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                                            <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true"><i class="fa fa-home pr-2"></i> Application</a>
-                                            <a class="nav-link" id="v-pills-report-tab" data-toggle="pill" href="#v-pills-report" role="tab" aria-controls="v-pills-report" aria-selected="false"><i class="fa fa-desktop pr-2"></i> Reports</a>
-                                            <a class="nav-link" id="v-pills-management-tab" data-toggle="pill" href="#v-pills-management" role="tab" aria-controls="v-pills-management" aria-selected="false"><i class="fa fa-magnet pr-2"></i>Management</a>
-                                            <a class="nav-link" id="v-pills-blog-tab" data-toggle="pill" href="#v-pills-blog" role="tab" aria-controls="v-pills-blog" aria-selected="false"><i class="fa fa-comments-o pr-2"></i>Blog</a>
-                                        </div>
-                                    </div>
-                                    <div class="col-8">
-                                        <div class="tab-content" id="v-pills-tabContent">
-                                            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                                                <ul class="list-unstyled b-mega-menu-link">
-                                                    <li><a href="#">Bootstrap 4 Stable</a></li>
-                                                    <li class="active"><a href="javascript:;">DashLab Modern Admin</a></li>
-                                                    <li><a href="#">Awesome Widgets Collection</a></li>
-                                                    <li><a href="#">Developer Friendly Code</a></li>
-                                                    <li><a href="#">SASS and GULP Task</a></li>
-                                                    <li><a href="#">Fully Responsive</a></li>
-                                                </ul>
-                                            </div>
-                                            <div class="tab-pane fade" id="v-pills-report" role="tabpanel" aria-labelledby="v-pills-report-tab">
-                                                <ul class="list-unstyled b-mega-menu-link">
-                                                    <li><a href="#">Daily Reports</a></li>
-                                                    <li><a href="javascript:;">Weekly Reports</a></li>
-                                                    <li class="active"><a href="#">Monthly Reports</a></li>
-                                                    <li><a href="#">Yearly Reports</a></li>
-                                                    <li><a href="#">HR Reports</a></li>
-                                                    <li><a href="#">Product Reports</a></li>
-                                                    <li><a href="#">Order Reports</a></li>
-                                                    <li><a href="#">Revenue Reports</a></li>
-                                                </ul>
-                                            </div>
-                                            <div class="tab-pane fade" id="v-pills-management" role="tabpanel" aria-labelledby="v-pills-management-tab">
-                                                <ul class="list-unstyled b-mega-menu-link">
-                                                    <li><a href="#">HR Management</a></li>
-                                                    <li class="active"><a href="javascript:;">Product Management</a></li>
-                                                    <li><a href="#">Role Management</a></li>
-                                                    <li><a href="#">Sales Management</a></li>
-                                                    <li><a href="#">Employee Management</a></li>
-                                                </ul>
-                                            </div>
-                                            <div class="tab-pane fade" id="v-pills-blog" role="tabpanel" aria-labelledby="v-pills-blog-tab">
-                                                <ul class="list-unstyled b-mega-menu-link">
-                                                    <li class="active"><a href="#">Educational Blog</a></li>
-                                                    <li> <a href="javascript:;">Technology Blog</a></li>
-                                                    <li><a href="#">Political Blog</a></li>
-                                                    <li><a href="#">Woocommerce Blog</a></li>
-                                                    <li><a href="#">Entertainment Blog</a></li>
-                                                    <li><a href="#">Newspapper Blog</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="tab-pane fade" id="analytics" role="tabpanel" aria-labelledby="analytics-tab">
-                                <div class="p-5 m-4 text-center">
-                                    <i class="fa fa-desktop f50 text-muted mb-4"></i>
-                                    <p class="mb-5">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC</p>
-                                    <a href="#" class="btn btn-primary">Get Started</a>
-                                </div>
-                            </div>
-                            <div class="tab-pane fade" id="application" role="tabpanel" aria-labelledby="application-tab">
-                                <div class="p-5 m-4 text-center">
-                                    <i class="fa fa-magnet f50 text-muted mb-4"></i>
-                                    <p class="mb-5">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33</p>
-                                    <a href="#" class="btn btn-purple">Get Started</a>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--/brand mega menu-->
 
         <!--responsive nav toggle-->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -211,24 +109,24 @@
 
                 <li class="nav-item {{ Request::routeIs('barang.*') ? 'active' : '' }}" data-toggle="tooltip" data-placement="right" title="Calendar">
                     <a class="nav-link" href="{{ route('barang.index') }}">
-                        <i class="vl_calendar"></i>
+                        <i class="fa fa-cubes"></i>
                         <span class="nav-link-text">Data Produk</span>
                     </a>
                 </li>
 
                 <li class="nav-item {{ Request::routeIs('proyek.*') ? 'active' : '' }}" data-toggle="tooltip" data-placement="right" title="Calendar">
                     <a class="nav-link" href="{{ route('proyek.index') }}">
-                        <i class="vl_calendar"></i>
+                        <i class="fa fa-building"></i>
                         <span class="nav-link-text">Data Proyek</span>
                     </a>
                 </li>
 
-                <li class="nav-item {{ Request::routeIs('pelanggan.*') ? 'active' : '' }}" data-toggle="tooltip" data-placement="right" title="Calendar">
+                {{-- <li class="nav-item {{ Request::routeIs('pelanggan.*') ? 'active' : '' }}" data-toggle="tooltip" data-placement="right" title="Calendar">
                     <a class="nav-link" href="{{ route('pelanggan.index') }}">
-                        <i class="vl_calendar"></i>
+                        <i class="fa fa-users"></i>
                         <span class="nav-link-text">Data Pelanggan</span>
                     </a>
-                </li>
+                </li> --}}
 
             </ul>
             <!--/header leftside links-->
@@ -304,7 +202,11 @@
                         <a class="dropdown-item" href="#">Inbox <span class="badge badge-primary">3</span></a>
                         <a class="dropdown-item" href="#">Message <span class="badge badge-success">5</span></a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="login.html">Sign Out</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">Sign Out</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </div>
                 </li>
             </ul>
